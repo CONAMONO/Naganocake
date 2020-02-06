c1a14916b4b2e3a076198aa90fd36c5c132fe52f
Taiyo <59132967+FasterRising01@users.noreply.github.com>
2020-02-06 16:42:55 +0900

    Update order.rb

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,13 @@
 class Order < ApplicationRecord
 	belongs_to :user
+	
+	validates :order_day presence: true
+	validates :total_price presence: true
+	validates :pay presence: true
+	validates :name_address presence: true
+	validates :street_address presence: true
+	validates :postal_code presence: true
+	validates :payment_method presence: true
+	validates :order_status presence: true
+
 end
