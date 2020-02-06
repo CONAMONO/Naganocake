bd57215968e284784259b82c0257ff9b066b597b
core-melt <core.melt21@gmail.com>
2020-02-06 16:00:49 +0900

    Update cart_item order_product Setting validates

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,2 +1,7 @@
 class CartItem < ApplicationRecord
+	belongs_to :user
+	belongs_to :product
+	validates :quantity, presence: true
+	validates :taxed_price, presence: true
+	validates :production_status, presence: true
 end
