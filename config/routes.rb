c71aec54fc01fcacc0ddf3fda8b7f279e186482f
admjz <admjz1411@gmail.com>
2020-02-06 09:37:49 +0000

    routing設定しました

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,27 @@
 Rails.application.routes.draw do
   devise_for :admins
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :admin do
+  	resources :genres, only: [:index,:create,:edit,:update]
+  	resources :users, only: [:index,:edit,:show,:update]
+  	resources :products, only: [:index,:create,:show,:new,:edit,:update]
+  	resources :orders, only: [:index,:show,:update]
+  	get "top"  => "orders#top"
+  end
+
+  namespace :public do
+	resource :users, only: [:create, :show, :edit, :update]
+	get "/users/confirm"  => "users#confirm"
+	patch "/users/confirm" => "users#update"
+	resources :products, only: [:index, :show]
+	resources :orders, only: [:index, :show, :new, :create]
+	get "/orders/confirm"  => "orders#confirm"
+	post "/orders"  => "orders#create"
+	get "/orders/thanks"  => "users#thanks"
+	resources :shipping_addresses, only: [:index, :create, :edit, :update, :destroy]
+	resources :cart_items, only: [:create, :index, :update, :destroy]
+	delete "/cart_items"  => "cart_items#destroy"
+	root to: "orders#top"
+  end
+	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
