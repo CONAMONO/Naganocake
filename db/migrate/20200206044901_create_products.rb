6eef2f363bdfe3aabf5b1cbd85e2363620354a81
admjz <admjz1411@gmail.com>
2020-02-08 03:48:00 +0000

    migration/orderの内容追記

--- db/migrate/20200206044901_create_products.rb
+++ db/migrate/20200206044901_create_products.rb
@@ -1,7 +1,11 @@
 class CreateProducts < ActiveRecord::Migration[5.2]
   def change
     create_table :products do |t|
-
+	  t.string :name
+      t.string :image_id
+      t.text :explain
+      t.integer :non_taxed_price
+      t.boolean :sale_status
       t.timestamps
     end
   end
