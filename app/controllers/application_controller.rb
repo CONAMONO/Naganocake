863cac5b76d1790b29863c7f1be4fde847e4d512
KAGIREN <kr.vb.cook@icloud.com>
2020-02-08 10:36:10 +0000

    add update sign_in sign_out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,17 @@
 class ApplicationController < ActionController::Base
 	def after_sign_in_path_for(resource)
-	    admin_top_path
+		if admin_signed_in?
+	    　　admin_top_path
+	    else
+	    	root_path
+	    end
+	end
+
+	def after_sign_out_path_for(resource)
+		if admin_signed_in?
+	    　　new_admin_session_path
+	    else
+    	    root_path
+    	end
 	end
 end
