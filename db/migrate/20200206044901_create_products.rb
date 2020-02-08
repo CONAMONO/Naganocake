8010507ad44f5752535a8fdd32b446d2fd38beae
admjz <admjz1411@gmail.com>
2020-02-08 08:27:27 +0000

    admin/top画面作成(注文件数反映は後程)

--- db/migrate/20200206044901_create_products.rb
+++ db/migrate/20200206044901_create_products.rb
@@ -1,7 +1,7 @@
 class CreateProducts < ActiveRecord::Migration[5.2]
   def change
     create_table :products do |t|
-	  t.string :name
+	    t.string :name
       t.string :image_id
       t.text :explain
       t.integer :non_taxed_price
