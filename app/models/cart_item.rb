7cc6be351e0fb14719254c8b81421238f16e222c
core-melt <core.melt21@gmail.com>
2020-02-11 14:46:18 +0900

    Update models validates and has_one->has_many

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,5 +1,7 @@
 class CartItem < ApplicationRecord
 	belongs_to :user
 	belongs_to :product
+	validates :product_id, presence: true
+	validates :user_id, presence: true
 	validates :quantity, presence: true
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,6 +2,7 @@ class Order < ApplicationRecord
 	belongs_to :user
 	has_many :order_products, dependent: :destroy
 	
+	validates :user_id, presence: true
 	validates :order_day presence: true
 	validates :total_price presence: true
 	validates :pay presence: true
