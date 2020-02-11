f7a6d284135b4c94a41e02a85d31c5b68832f38a
core-melt <59461531+core-melt@users.noreply.github.com>
2020-02-11 15:05:50 +0900

    Update product.rb

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
-  has_many :cart_item,  dependent: :destroy
-  has_many :order_product,  dependent: :destroy
+  has_many :cart_items,  dependent: :destroy
+  has_many :order_products,  dependent: :destroy
   belongs_to :genre
   attachment :image
 
