779ee785dbc0c0126db3e3de76a1d9b5a5abbd97
KAGIREN <kr.vb.cook@icloud.com>
2020-02-12 04:05:22 +0000

    add controller order

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,5 +1,24 @@
 class Admin::OrdersController < ApplicationController
   def top
   end
-  
+
+  def index
+  	@orders = Order.all
+  end
+
+  def show
+  	@order = Order.find(params[:id])
+    @order_product = Order_Product.find(@order.order_product.id)
+  end
+
+  private
+  def order_params
+		params.require(:order).permit(:user_id, :order_day, :total_price, :pay, :name_address, :street_address, :postal_code,
+			:payment_method,:order_status)
+	end
+
+  def order_product_params
+    params.require(:order_product).permit(:order_id, :product_id, :quantity, :taxed_price, :production_status)
+  end
+
 end
