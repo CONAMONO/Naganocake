1197208df1d6697bb1f0f1c2020912b6c8c66a62
T4iyo <taiyosun.0826@gmail.com>
2020-02-12 14:48:01 +0900

    商品一覧及び商品詳細画面作成

--- app/controllers/public/shipping_addresses_controller.rb
+++ app/controllers/public/shipping_addresses_controller.rb
@@ -1,10 +1,21 @@
 class Public::ShippingAddressesController < ApplicationController
 	def index
-		@shipping_addresses = ShippingAddress.all
+		@shipping_addresses = User.find(current_user.id)
+		@sh_addresses = @shipping_addresses.shipping_addresses
 		@shipping_address = ShippingAddress.new
 	end
 
 	def edit
+		@shipping_addresses = ShippingAddress.find(params[:id])
+		if @shipping_addresses.user_id != current_user.id
+			redirect_to public_shipping_addresses_path
+		end
+	end
+
+	def update
+		@shipping_address = ShippingAddress.find(params[:id])
+		@shipping_address.update(shipping_address_params)
+		redirect_to public_shipping_addresses_path
 	end
 
 	def create
@@ -22,10 +33,6 @@ class Public::ShippingAddressesController < ApplicationController
 		@shipping_address = ShippingAddress.new
 	end
 
-	def update
-		@shipping_address = @ShippingAddress.find(params[:id])
-		@shipping_address.update(shipping_address_params)
-	end
 
 	def destroy
 		shipping_address = ShippingAddress.find(params[:id])
