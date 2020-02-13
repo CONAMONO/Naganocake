ef5b048c7e9e525d4c98e42b5cf42b7ad71c1655
KAGIREN <kr.vb.cook@icloud.com>
2020-02-13 07:46:17 +0000

    add create controller poblic order

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,4 +1,16 @@
 class Public::OrdersController < ApplicationController
 	def top
 	end
+	def index
+		@user = User.find(current_user.id)
+	  	@orders = @user.orders
+	end
+	def show
+		@order = Order.find(params[:id])
+	    @order_products = @order.order_products
+	    @products = Product.all
+	end
+
+	private
+
 end
