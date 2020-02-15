ddcd5dd5e0123d0a88f510870452e98241a1bc70
core-melt <core.melt21@gmail.com>
2020-02-15 19:04:17 +0900

    Update Errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :user_status_check!
   before_action :configure_permitted_parameters, if: :devise_controller?
 	def after_sign_in_path_for(resource)
 		if admin_signed_in?
@@ -17,11 +18,25 @@ class ApplicationController < ActionController::Base
     	    root_path
     	end
 	end
+
   protected
   def configure_permitted_parameters
   	added_attrs = [ :first_name, :last_name, :first_name_kata, :last_name_kata, :street_address, :postal_code, :phone_number, :user_status, :email, :password, :password_confirmation]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
     devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
+  end
+
+
+  private
+  def user_status_check!
+  	if user_signed_in?
+  		if current_user.user_status
+	  		sign_out
+	    	flash[:alert] = "このユーザーは退会しています"
+	    	redirect_to root_path
+    	end
+  	end
+
   end
 end
