191e74ca6ce859df9f479fde9092cd0c8325dbd6
KAGIREN <kr.vb.cook@icloud.com>
2020-02-15 04:43:41 +0000

    update controllers public order

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,16 +1,102 @@
 class Public::OrdersController < ApplicationController
+
+
 	def top
+		@products = Product.all
+		@products_count = Product.select("id").count
+		@product_name = Product.select("name")
+		@product_price = Product.select("non_taxed_price")
 	end
+
+	def new
+		#@address_select = {:ご自身の住所=>1, :登録済住所から選択=>2, :新しいお届け先=>3}
+		@order = Order.new
+		@shipping_addresses = current_user.shipping_addresses.all
+		@user = User.find(current_user.id)
+	end
+
+
 	def index
 		@user = User.find(current_user.id)
 	  	@orders = @user.orders
+	  	@order_products = OrderProduct.all
+	  	@products = Product.all
 	end
+
 	def show
 		@order = Order.find(params[:id])
 	    @order_products = @order.order_products
 	    @products = Product.all
 	end
 
-	private
 
+	def create
+	  tax = 1.10
+	  @cart_items = current_user.cart_items.all
+      order = Order.new(order_params)
+      #order.pay = 800
+      #order.total = @total
+
+      if order.save == true
+      	#flash[:success] = 'You have creatad book successfully.'
+		order_product = OrderProduct.new
+
+		@cart_items.each do |cart_item|
+			order_product.product_count = cart_item.quantity
+			order_product.taxed_price = ((cart_item.product.non_taxed_price * tax) * cart_item.quantity).floor
+			order_product.production_status = 0
+			order_product.order_id = order.id
+			order_product.product_id = cart_item.product_id
+
+			if order_product.save == false
+				@error += "・" + cart_item.id + "が正常に登録できませんでした"
+				render action: :confirm
+				return
+			end
+		end
+
+        redirect_to public_orders_thanks_path and return
+      else
+
+		@products = Product.all
+		@order = Order.new
+		@user = User.find(current_user.id)
+		@shipping_addresses = current_user.shipping_addresses.all
+		@payment_method = params[:payment_method]
+		@address_id = params[:address_id]
+		@select_addr = params[:select_addr]
+		@new_postal_code = params[:new_postal_code]
+		@new_street_address = params[:new_street_address]
+		@new_name = params[:new_name]
+        render action: :confirm
+      end
+	end
+
+	def confirm
+		@cart_items = current_user.cart_items.all
+		@products = Product.all
+		@order = Order.new
+		@user = User.find(current_user.id)
+		@shipping_addresses = current_user.shipping_addresses.all
+		@payment_method = params[:payment_method]
+		@address_id = params[:address_id]
+		@select_addr = params[:select_addr]
+		@new_postal_code = params[:new_postal_code]
+		@new_street_address = params[:new_street_address]
+		@new_name = params[:new_name]
+	end
+
+	def thanks
+	end
+
+    private
+    def order_params
+    	added_attrs = [:order_day, :user_id, :total_price, :pay, :name_address, :street_address, :postal_code, :payment_method, :order_status]
+        params.require(:order).permit(added_attrs)
+    end
+
+    def order_product_params
+    	added_attrs = [:product_count, :taxed_price, :production_status, :order_id, :product_id]
+        params.require(:order).permit(added_attrs)
+    end
 end
