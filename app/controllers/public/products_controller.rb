8a156c1895404ad014f455b2f91c2423a86b138e
T4iyo <taiyosun.0826@gmail.com>
2020-02-19 19:19:20 +0900

    fixed moving and CSS

--- app/controllers/public/products_controller.rb
+++ app/controllers/public/products_controller.rb
@@ -5,10 +5,10 @@ class Public::ProductsController < ApplicationController
 		@genres = Genre.where(status: "1")
 		# urlにgenre_idがある場合
 		if params[:genre_id]
-			@genre = Genre.find(params[:genre_id])
+			@genre = Genre.find_by(params[:genre_id])
 			# 降順で表示
 			@products = Product.where(genre_id: params[:genre_id]).page(params[:page]).per(PER)
-			@products_count = @products.count
+			@products_count = @genre.products.where(sale_status: "1").count
 		else
 			@products_count = Genre.select("id").count
 			@products = Product.all
